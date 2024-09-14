78d046b4518be9333b7f5c098ed248069839e778
DmitryCo <dm3ypost@gmail.com>
2024-09-14 19:14:59 +0300

    Add application in the build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,19 +1,24 @@
 plugins {
-    id 'java'
+    id("java")
+    application
 }
 
-group = 'hexlet.code'
-version = '1.0-SNAPSHOT'
+application {
+    mainClass = "hexlet.code.App"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation platform('org.junit:junit-bom:5.10.0')
-    testImplementation 'org.junit.jupiter:junit-jupiter'
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
-test {
+tasks.test {
     useJUnitPlatform()
 }
