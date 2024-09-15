37176902e761061fbf47f5e75f89f59cee4ee623
DmitryCo <dm3ypost@gmail.com>
2024-09-15 18:53:33 +0300

    Add parameters for file paths and format option

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -8,7 +8,17 @@ import picocli.CommandLine.Parameters;
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
 
-final class App implements Runnable {
+class App implements Runnable {
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private String filepath1;
+
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private String filepath2;
+
+    @Option(names = {"-f", "--format"}, paramLabel = "format", defaultValue = "stylish",
+            description = "output format [default: stylish]")
+    private String format;
+
     @Override
     public void run() {
 
