6fb7378073285d49d8a46271a3f95488465b662f
DmitryCo <dm3ypost@gmail.com>
2024-09-19 21:55:34 +0300

    Add generate(), validateFileExists(), readFile(), log()

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,2 +1,44 @@
-package hexlet.code;public class Differ {
+package hexlet.code;
+
+import java.io.File;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
+
+public class Differ {
+    private static final Logger logger = Logger.getLogger(Differ.class.getName());
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static void generate(String filepath1, String filepath2, String format) throws Exception {
+        Path firstFile = Paths.get(filepath1).toAbsolutePath().normalize();
+        Path secondFile = Paths.get(filepath2).toAbsolutePath().normalize();
+
+        validateFileExists(firstFile, filepath1);
+        validateFileExists(secondFile, filepath2);
+
+        try {
+            Map<String, Object> contentFirstFile = readFile(firstFile.toFile());
+            Map<String, Object> contentSecondFile = readFile(secondFile.toFile());
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "File not created", e);
+            throw e;
+        }
+    }
+
+    private static void validateFileExists(Path absFilePath, String relFilePath) throws Exception {
+        if (!Files.exists(absFilePath)) {
+            throw new Exception("File '" + relFilePath + "' does not exist");
+        }
+    }
+
+    private static Map<String, Object> readFile(File file) throws Exception {
+        return objectMapper.readValue(file, new TypeReference<Map<String, Object>>() {
+        });
+    }
 }
