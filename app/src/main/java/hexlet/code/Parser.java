87af13751cb1699f4a6c1797ba60f8b6fa77f2d9
DmitryCo <dm3ypost@gmail.com>
2024-10-01 22:32:41 +0300

    Add .yaml parser in the Parser.java

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -2,6 +2,7 @@ package hexlet.code;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
 import java.io.File;
 import java.nio.file.Files;
@@ -9,18 +10,26 @@ import java.nio.file.Path;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
+import java.io.IOException;
 
 public class Parser {
     private static final Logger LOGGER = Logger.getLogger(Parser.class.getName());
-    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private static final ObjectMapper JSON_OBJECT_MAPPER = new ObjectMapper();
+    private static final ObjectMapper YAML_OBJECT_MAPPER = new ObjectMapper(new YAMLFactory());
 
     public static Map<String, Object> parsing(Path absFilePath, String relFilePath) throws Exception {
         validateFileExists(absFilePath, relFilePath);
+        String fileFormat = getFileFormat(relFilePath);
         Map<String, Object> contentFile;
 
         try {
-            contentFile = readFile(absFilePath.toFile());
+            if ("yaml".equalsIgnoreCase(fileFormat) || "yml".equalsIgnoreCase(fileFormat)) {
+                contentFile = readYamlFile(absFilePath.toFile());
+            } else if ("json".equalsIgnoreCase(fileFormat)) {
+                contentFile = readJsonFile(absFilePath.toFile());
+            } else {
+                throw new IllegalArgumentException("Unsupported file format: " + fileFormat);
+            }
         } catch (Exception e) {
             LOGGER.log(Level.SEVERE, "File not created", e);
             throw e;
@@ -34,8 +43,17 @@ public class Parser {
         }
     }
 
-    private static Map<String, Object> readFile(File file) throws Exception {
-        return OBJECT_MAPPER.readValue(file, new TypeReference<Map<String, Object>>() {
+    private static String getFileFormat(String relFilePath) {
+        return relFilePath.substring(relFilePath.indexOf(".") + 1);
+    }
+
+    private static Map<String, Object> readJsonFile(File file) throws IOException {
+        return JSON_OBJECT_MAPPER.readValue(file, new TypeReference<Map<String, Object>>() {
+        });
+    }
+
+    private static Map<String, Object> readYamlFile(File file) throws IOException {
+        return YAML_OBJECT_MAPPER.readValue(file, new TypeReference<Map<String, Object>>() {
         });
     }
 }
