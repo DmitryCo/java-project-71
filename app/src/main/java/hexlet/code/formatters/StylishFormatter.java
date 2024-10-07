7cde7223d780826d632ccf91b0ce7b2f9a7bd0bd
DmitryCo <dm3ypost@gmail.com>
2024-10-08 00:30:30 +0300

    Add fixtures

--- app/src/main/java/hexlet/code/formatters/StylishFormatter.java
+++ app/src/main/java/hexlet/code/formatters/StylishFormatter.java
@@ -1,37 +1,53 @@
 package hexlet.code.formatters;
 
-import java.util.List;
-import hexlet.code.Diff;
+import hexlet.code.DiffTreeBuilder.ChangeType;
 
-public class FormatStylish {
-    public static String stylish(Diff diff) {
+import java.util.List;
+import java.util.Map;
+
+public class StylishFormatter {
+    public static String stylishFormat(List<Map<String, Object>> diffTree) {
         StringBuilder result = new StringBuilder("{\n");
-        for (Changes change : diff.getChanges()) {
-            String formattedLine = formatChanges(change);
-            result.append(formattedLine).append("\n");
+        for (Map<String, Object> diff : diffTree) {
+            ChangeType changeType = (ChangeType) diff.get("changeType");
+            switch (changeType) {
+                case REMOVED:
+                    result.append("  - ")
+                            .append(diff.get("key"))
+                            .append(": ")
+                            .append(diff.get("oldValue"))
+                            .append("\n");
+                    break;
+                case ADDED:
+                    result.append("  + ")
+                            .append(diff.get("key"))
+                            .append(": ")
+                            .append(diff.get("newValue"))
+                            .append("\n");
+                    break;
+                case UNCHANGED:
+                    result.append("    ")
+                            .append(diff.get("key"))
+                            .append(": ")
+                            .append(diff.get("oldValue"))
+                            .append("\n");
+                    break;
+                case CHANGED:
+                    result.append("  - ")
+                            .append(diff.get("key"))
+                            .append(": ")
+                            .append(diff.get("oldValue"))
+                            .append("\n");
+                    result.append("  + ")
+                            .append(diff.get("key"))
+                            .append(": ")
+                            .append(diff.get("newValue"))
+                            .append("\n");
+                    break;
+                default:
+                    throw new RuntimeException("Unknown difference tree type: " + diffTree);
+            }
         }
         return result.append("}").toString();
-    }
-
-    private static String formatChanges(Changes change) {
-        String prefix;
-        switch (change.typeOfChange) {
-            case ADDED:
-                prefix = "  + ";
-                break;
-            case REMOVED:
-                prefix = "  - ";
-                break;
-            case CHANGED:
-                prefix = "  - ";
-                break;
-            case UNCHANGED:
-                prefix = "    ";
-                break;
-            default:
-                prefix = "    ";
-                break;
-        }
-        return String.format("%s%s: %s", prefix, change.key, change.value);
     }
 }
