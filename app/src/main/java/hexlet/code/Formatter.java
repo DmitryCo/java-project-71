f19d5d6b8552ac50d4f62398f96d2b689d449088
DmitryCo <dm3ypost@gmail.com>
2024-10-08 15:58:48 +0300

    Add PlainFormatter

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -1,6 +1,7 @@
 package hexlet.code;
 
 import hexlet.code.formatters.StylishFormatter;
+import hexlet.code.formatters.PlainFormatter;
 
 import java.util.Map;
 import java.util.List;
@@ -10,6 +11,7 @@ public class Formatter {
     public static String format(List<Map<String, Object>> diffTree, String formatType) throws IOException {
         return switch (formatType) {
             case "stylish" -> StylishFormatter.stylishFormat(diffTree);
+            case "plain" -> PlainFormatter.plainFormat(diffTree);
             default -> throw new IllegalArgumentException("Unsupported format type: " + formatType);
         };
     }
---
+++ app/src/main/java/hexlet/code/formatters/PlainFormatter.java
@@ -0,0 +1,56 @@
+package hexlet.code.formatters;
+
+import hexlet.code.DiffTreeBuilder.ChangeType;
+
+import java.util.List;
+import java.util.Map;
+
+public class PlainFormatter {
+    public static String plainFormat(List<Map<String, Object>> diffTree) {
+        StringBuilder result = new StringBuilder();
+        for (Map<String, Object> diff : diffTree) {
+            ChangeType changeType = (ChangeType) diff.get("changeType");
+            switch (changeType) {
+                case REMOVED:
+                    result.append("Property '")
+                            .append(diff.get("key"))
+                            .append("' was removed")
+                            .append("\n");
+                    break;
+                case ADDED:
+                    result.append("Property '")
+                            .append(diff.get("key"))
+                            .append("' was added with value: ")
+                            .append(checkValue(diff.get("newValue")))
+                            .append("\n");
+                    break;
+                case UNCHANGED:
+                    break;
+                case CHANGED:
+                    result.append("Property '")
+                            .append(diff.get("key"))
+                            .append("' was updated. From ")
+                            .append(checkValue(diff.get("oldValue")))
+                            .append(" to ")
+                            .append(checkValue(diff.get("newValue")))
+                        .append("\n");
+                    break;
+                default:
+                    throw new RuntimeException("Unknown difference tree type: " + diffTree);
+            }
+        }
+        return result.toString().trim();
+    }
+
+    public static String checkValue(Object value) {
+        if (value instanceof String) {
+            return "'" + value + "'";
+        } else if (value == null) {
+            return null;
+        } else if (value instanceof List || value instanceof Map) {
+            return "[complex value]";
+        } else {
+            return value.toString();
+        }
+    }
+}
