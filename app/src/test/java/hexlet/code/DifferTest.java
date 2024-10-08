ec3433b6df70ef2bb5119f9f08fc06cc617a36a8
DmitryCo <dm3ypost@gmail.com>
2024-10-08 15:08:43 +0300

    Add stylish and default format tests

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -2,12 +2,57 @@ package hexlet.code;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.Path;
 
+
 public class DifferTest {
+    private static String expectedJSON;
+    private static String expectedStylish;
+    private static String file1JSON;
+    private static String file2JSON;
+    private static String file1YAML;
+    private static String file2YAML;
+
+    @BeforeAll
+    public static void beforeAll() throws Exception {
+        expectedJSON = readFixture("expectedJSON.json");
+        expectedStylish = readFixture("expectedStylish.txt");
+        file1JSON = getPath("file1.json");
+        file2JSON = getPath("file2.json");
+        file1YAML = getPath("file1.yml");
+        file2YAML = getPath("file2.yml");
+    }
+
+    @Test
+    public void testTreeJSON() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode expected = objectMapper.readTree(expectedJSON);
+        JsonNode actual = objectMapper.readTree(expectedJSON);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testStylishJSON() throws Exception {
+        assertEquals(expectedStylish, Differ.generate(file1JSON, file2JSON, "stylish"));
+    }
+
+    @Test
+    public void testStylishYAML() throws Exception {
+        assertEquals(expectedStylish, Differ.generate(file1YAML, file2YAML, "stylish"));
+    }
+
+    @Test
+    public void testDefaultFormat() throws Exception {
+        assertEquals(expectedStylish, Differ.generate(file1JSON, file2JSON));
+        assertEquals(expectedStylish, Differ.generate(file1YAML, file2YAML));
+    }
+
     private static Path getFixturePath(String fileName) {
         return Paths.get("src", "test", "resources", "fixtures", fileName)
                 .toAbsolutePath().normalize();
@@ -18,16 +63,7 @@ public class DifferTest {
         return Files.readString(path).trim();
     }
 
-    private String file1 = "file1.json";
-    private String file2 = "file2.json";
-    private String file3 = "file1.yml";
-    private String file4 = "file2.yml";
-
-    @Test
-    public void testStylishJson() throws Exception {
-        String format = "stylish";
-        String expected = readFixture("ExpectedStylishJsonJson.txt");
-        var actual = Differ.generate(file1, file2, format);
-        assertEquals(expected, actual);
+    public static String getPath(String fileName) {
+        return String.format("src/test/resources/fixtures/%s", fileName);
     }
 }
