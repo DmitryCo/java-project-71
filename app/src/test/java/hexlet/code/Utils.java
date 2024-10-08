1f73050146d61b87da20c05be1bf024eb38f9daf
DmitryCo <dm3ypost@gmail.com>
2024-10-08 18:20:45 +0300

    Move common test methods to Utils

--- app/src/test/java/hexlet/code/Utils.java
+++ app/src/test/java/hexlet/code/Utils.java
@@ -1,11 +1,16 @@
 package hexlet.code;
 
-    private static Path getFixturePath(String fileName) {
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+
+public class Utils {
+    public static Path getFixturePath(String fileName) {
         return Paths.get("src", "test", "resources", "fixtures", fileName)
                 .toAbsolutePath().normalize();
     }
 
-    private static String readFixture(String fileName) throws Exception {
+    public static String readFixture(String fileName) throws Exception {
         var path = getFixturePath(fileName);
         return Files.readString(path).trim();
     }
@@ -13,3 +18,4 @@ package hexlet.code;
     public static String getPath(String fileName) {
         return String.format("src/test/resources/fixtures/%s", fileName);
     }
+}
